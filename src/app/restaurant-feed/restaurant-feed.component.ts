3bed3b048f7774adbe18c600090c0c8fa181eb20
Hubert Lin <linhub15@gmail.com>
2019-07-11 20:53:02 -0600

    google map working

--- src/app/restaurant-feed/restaurant-feed.component.ts
+++ src/app/restaurant-feed/restaurant-feed.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {} from 'googlemaps';
 
 @Component({
   selector: 'app-restaurant-feed',
@@ -6,16 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./restaurant-feed.component.scss']
 })
 export class RestaurantFeedComponent implements OnInit {
-  public time = new Date();
-  public longitude;
-  public latitude;
+
+  @ViewChild('map', { static: true }) mapElement: any;
+
+  time = new Date();
+  location: google.maps.LatLng;
+  map: google.maps.Map;
 
   constructor() {}
 
   ngOnInit() {
-    navigator.geolocation.getCurrentPosition(position => {
-      this.longitude = position.coords.longitude;
-      this.latitude = position.coords.latitude;
-    })
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.location = new google.maps.LatLng(
+          position.coords.latitude,
+          position.coords.longitude
+        );
+        const mapProp: google.maps.MapOptions = {
+          center: this.location,
+          zoom: 15,
+          mapTypeId: google.maps.MapTypeId.ROADMAP,
+          disableDefaultUI: true
+
+        };
+        this.map = new google.maps.Map(this.mapElement.nativeElement, mapProp);
+        this.setMarker(this.location);
+      });
+    } else {
+      alert('Geolocation is not supported by this browser.');
+    }
+  }
+
+  setMarker(location: google.maps.LatLng) {
+    new google.maps.Marker({
+      position: location,
+      map: this.map
+    });
   }
 }
